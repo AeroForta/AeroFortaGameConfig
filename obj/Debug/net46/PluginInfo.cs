0d25601e8d206e9582b848b3172cbb98f9e870f9
Δєяσ_fσятα <86554858+AeroForta@users.noreply.github.com>
2024-01-17 20:11:37 +0700

    Renaming, fixing, and rebuilding

--- obj/Debug/net46/PluginInfo.cs
+++ obj/Debug/net46/PluginInfo.cs
@@ -1,9 +1,9 @@
-namespace AeroFortaGameConfig
+namespace AeroFortaGameObjectEditor
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "AeroFortaGameConfig";
-        public const string PLUGIN_NAME = "AeroFortaGameConfig";
+        public const string PLUGIN_GUID = "AeroFortaGameObjectEditor";
+        public const string PLUGIN_NAME = "AeroFortaGameObjectEditor";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
